98859887652b8c5f70e36ca496a3ca951cb95c18
Jérémy Christillin <jeremy@neoservice.ai>
2025-12-14 14:05:27 +0100

    fix(bank-import): register list JS in hooks.py

--- mint/hooks.py
+++ mint/hooks.py
@@ -44,7 +44,9 @@ add_to_apps_screen = [
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+doctype_list_js = {
+	"Mint Bank Statement Import": "mint/doctype/mint_bank_statement_import/mint_bank_statement_import_list.js"
+}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
